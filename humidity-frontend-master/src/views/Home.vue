b9b991aacdd965557a4627ff62f9b1df31d4ae6c
passumtornado <39583908+passumtornado@users.noreply.github.com>
2020-09-14 08:54:08 +0000

    Create Home.vue

---
+++ humidity-frontend-master/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+   <br>
+    <Navbar />
+    <br>
+  <Jumbotron />
+ </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+import Jumbotron from '../components/Jumbotron'
+export default {
+  components: {
+    Navbar,
+    Jumbotron
+  }
+}
+</script>
+
+<style>
+
+</style>
